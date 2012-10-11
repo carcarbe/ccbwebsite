337ad64cf7abca6e40d28790aedc63df7244906d
carcarbe <carcarbe@gmail.com>
2012-10-11 12:26:17 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ccbwebsite::Application.config.secret_token = 'dc317f1c7d5db3009ae2d1fd1480baca8616284d49c12474f47c88cf27ecf116482287eabf409025445f34556198c7c0f6f9f7998272ef756c06e0499cf3970a'
