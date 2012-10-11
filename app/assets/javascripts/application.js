5b018c19e31034882a57ac93496f5cdc4b9876d4
carcarbe <carcarbe@gmail.com>
2012-10-11 16:25:49 +0200

    Add css and javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,39 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+function check_info_type()
+{
+	var selected_text = $('#content_kind option:selected').text()
+		
+		if(selected_text=="Image") {
+		$('#text_info').hide();
+		$('#image_info').show();	
+		}
+		else
+		if(selected_text=="Text")
+		{
+		$('#image_info').hide();
+		$('#text_info').show();	
+		}
+		
+}
+
+$(document).ready(function() {
+	//$('#mainNav a').click(function() {
+	// var url=$(this).attr('href');
+	// $('webContent').empty();
+	//$('#webContent').load(url+ ' #webContent');
+	
+
+	//return false;
+//});
+
+	 
+	
+	check_info_type();
+	$('#content_kind').change( function() { 
+		check_info_type();
+});
+});
+
